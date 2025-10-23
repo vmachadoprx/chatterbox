9faf5a1fa2c7898a175c3bd4c88ebabaa35c75c5
vmachadoprx <victormachado@cohere.com>
2025-10-23 23:56:59 +0000

    revert output attns

--- src/chatterbox/models/t3/t3.py
+++ src/chatterbox/models/t3/t3.py
@@ -323,7 +323,7 @@ class T3(nn.Module):
             inputs_embeds=inputs_embeds,
             past_key_values=None,
             use_cache=True,
-            output_attentions=False,
+            output_attentions=True,
             output_hidden_states=True,
             return_dict=True,
         )
@@ -382,7 +382,7 @@ class T3(nn.Module):
             output = self.patched_model(
                 inputs_embeds=next_token_embed,
                 past_key_values=past,
-                output_attentions=False,
+                output_attentions=True,
                 output_hidden_states=True,
                 return_dict=True,
             )
